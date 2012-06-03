3511a3a56ce726a3a77ba4f03fc0ab465198ca27
Dmitry Lavrov <lavrovdv@gmail.com>
2012-06-03 09:13:22 +0400

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: I
+ * Date: 01.06.12
+ * Time: 20:45
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(function() {
+    $('#add-vacation').click(function(){$('#myModal').modal();});
+    $('.typeahead').typeahead()
+});
